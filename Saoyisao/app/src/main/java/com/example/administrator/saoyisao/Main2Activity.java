0c2bec7ca306c3b14959ff14145540abfebb2499
ZeErdada <1371598840@qq.com>
2017-08-23 20:37:54 +0800

    二维码扫描基础实现

---
+++ Saoyisao/app/src/main/java/com/example/administrator/saoyisao/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.saoyisao;
+
+import android.os.Bundle;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Main2Activity extends CaptureActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Saoyisao/app/src/main/java/com/example/administrator/saoyisao/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.administrator.saoyisao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button)
+    Button button;
+    @Bind(R.id.activity_main)
+    RelativeLayout activityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                // 设置要扫描的条码类型，ONE_D_CODE_TYPES：一维码，QR_CODE_TYPES-二维码
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setCaptureActivity(Main2Activity.class);
+                integrator.setPrompt("请扫描二维码"); //底部的提示文字，设为""可以置空
+                integrator.setCameraId(0); //前置或者后置摄像头
+                integrator.setBeepEnabled(false); //扫描成功的「哔哔」声，默认开启
+                integrator.setBarcodeImageEnabled(true);//是否保留扫码成功时候的截图
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String result = scanResult.getContents();
+            Log.e("HYN", result);
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+    }
+}
